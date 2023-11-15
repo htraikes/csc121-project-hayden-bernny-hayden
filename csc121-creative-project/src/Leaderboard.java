17bdc1e85fc97b4bb18a9fa1db37d5d3dd1fdd3c
htraikes <104409683+htraikes@users.noreply.github.com>
2023-11-15 14:13:02 -0500

    almost finished leaderboard

--- csc121-creative-project/src/Leaderboard.java
+++ csc121-creative-project/src/Leaderboard.java
@@ -3,17 +3,23 @@ import java.util.*;
 import processing.core.*;
 
 public class Leaderboard extends Enemies{
-	public static Dictionary<Integer, String> records = new Hashtable<>();
+	public static Dictionary<String, Integer> records = new Hashtable<>();
+	public ArrayList<Integer> scores;
 	
 	
-	Leaderboard(Dictionary<Integer, String> records){
+	Leaderboard(Dictionary<String, Integer> records, ArrayList<Integer> scores){
 		Leaderboard.records = records;
-		super(Enemies);
+		this.scores = scores;
+		super(enemyImg, explosion);
 	}
 	
-	public void draw(PApplet p) {
-		//sort the dictionary, then draw it
-		Collections.sort((Dictionary<Integer,String>) records);
+	public PApplet draw(PApplet p) {
+		//place items into scores Arraylist, sort it, then draw it
+		for (int i = 0; i < records.size(); i++) {
+			scores.add(records.get(i));
+		}
+		Collections.sort(scores);
+		return p;
 	}
 	
 	public Leaderboard update() {
@@ -22,13 +28,20 @@ public class Leaderboard extends Enemies{
 		//scanner for getting name
 		Scanner kb = new Scanner(System.in);
 		String name = kb.nextLine();
-		records.put(score, name);
+		int score = getScore();
+		records.put(name, score);
 		
 		//print out record to leaderboard.txt
-		PrintWriter pw = new PrintWriter("leaderboard.txt");
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter("leaderboard.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		pw.println(score + " " + name);
 			
-		
+		return this;
 		//open output file
 	}
 }
